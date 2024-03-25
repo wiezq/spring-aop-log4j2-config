f48b8a0bcf13f8558fed02e70f6801bf679033f7
Ivan Maltsev <53794764+wiezq@users.noreply.github.com>
2024-03-25 03:38:05 +0300

    Add files via upload

---
+++ src/main/java/com/example/t1_task3/T1Task3Application.java
@@ -0,0 +1,13 @@
+package com.example.t1_task3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1Task3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1Task3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/t1_task3/T1Task3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t1_task3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1Task3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
